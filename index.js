938d0f84f38125ea952629b3b6d77fa6bc9a5dfa
Neaz-Mahmood <neazn694@gmail.com>
2021-03-07 14:22:26 +0600

    removed quete from body parser line

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require(body-parser);
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
