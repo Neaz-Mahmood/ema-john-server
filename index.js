ce9538f5e2e34a74045a2352dca9d2f4a0a180a4
Neaz <neazn694@gmail.com>
2021-03-17 16:16:18 +0600

    removed the root api

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.use(bodyParser.json());
 app.use(cors())
 const port = 5000
 
-app.get('/', (req, res) =>{
-  res.send("It's working")
-})
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
